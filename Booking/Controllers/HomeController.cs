39d38a07a68d82504183525edd8e5fd6f8802358
Borko <boburche123@gmail.com>
2025-05-10 10:55:33 +0300

    Add facility count display and new feature section to Home Index view

--- Booking/Controllers/HomeController.cs
+++ Booking/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using Booking.Data;
+using Booking.Data.Entities;
 using Booking.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,14 +9,17 @@ namespace Booking.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            ViewBag.FacilityCount = _context.Facilities.Count();
             return View();
         }
 
